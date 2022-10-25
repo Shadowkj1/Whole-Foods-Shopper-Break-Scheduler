21d8d031870b6c9cdc27ff652e748b5fc09722e8
Shadowkj1 <41036282+Shadowkj1@users.noreply.github.com>
2022-10-25 05:35:47 -0400

    this is a test push

---
+++ lib/pages/home.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[400],
+      body: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+                width: 200,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/Schedule');
+                    },
+                    child: Text('button 1'))),
+            SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+                width: 200,
+                height: 40,
+                child:
+                    ElevatedButton(onPressed: () {}, child: Text('button 2'))),
+            SizedBox(
+              height: 5,
+            ),
+          ])
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/pages/timer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Timer extends StatefulWidget {
+  const Timer({super.key});
+
+  @override
+  State<Timer> createState() => _TimerState();
+}
+
+class _TimerState extends State<Timer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('this is the timer screen~'),
+      ),
+    );
+  }
+}
