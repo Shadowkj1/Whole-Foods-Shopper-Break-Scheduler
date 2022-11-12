86800f3abbbd83a74a3a4d7f4e58aab710550752
Shadowkj1 <41036282+Shadowkj1@users.noreply.github.com>
2022-11-12 02:23:19 -0500

    Timer up. only one person can break. used streams

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_print, dead_code
 
 import 'package:amazonbreak/pages/login2.dart';
 import 'package:amazonbreak/pages/schedule.dart';
@@ -9,13 +9,35 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   String? userName;
+
   String? shiftFormat;
+
+  bool? canIBreak;
+  bool? verify;
+
   DateTime? shift;
+
   final timeIsNow = DateTime.now();
+
+  DocumentReference breakRefforStream =
+      FirebaseFirestore.instance.collection("Shoppers").doc("breakActivity");
+
+  late Stream<DocumentSnapshot> _streamBreak;
+
+  @override
+  void initState() {
+    super.initState();
+    _streamBreak = breakRefforStream.snapshots();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +45,9 @@ class Home extends StatelessWidget {
     //making an reference to the instance
     DocumentReference realRef =
         FirebaseFirestore.instance.collection("Shoppers").doc(aUser!.uid);
+
+    DocumentReference breakRef =
+        FirebaseFirestore.instance.collection("Shoppers").doc("breakActivity");
     String UID = aUser.uid;
 ////////////////////////////////////////
     // get User (or so we may say~)
@@ -61,6 +86,9 @@ class Home extends StatelessWidget {
                     );
                   }),
             ),
+
+/////////////////////////////////////////////////////////////////////
+            //this is all the junk text at the top of the screen
             Container(
               alignment: Alignment(0, -1),
               child: Column(
@@ -85,10 +113,21 @@ class Home extends StatelessWidget {
                           return Text('Loading....');
                         }
                         return Text('shift time: ....nothing');
+                      }),
+                  FutureBuilder(
+                      future: isBreakActive(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState != ConnectionState.done) {
+                          return Text('Loading....');
+                        }
+                        return Text('$canIBreak');
                       })
                 ],
               ),
             ),
+/////////////////////////////////////////////////////////////////////////
+            ///
+            // this is the todays schedule button
             Container(
               alignment: Alignment(0, -1),
               child: Column(
@@ -115,6 +154,7 @@ class Home extends StatelessWidget {
                         child:
                             Text('Schedule', style: TextStyle(fontSize: 23))),
                   ),
+
                   SizedBox(
                     height: 40,
                   ),
@@ -123,33 +163,162 @@ class Home extends StatelessWidget {
                     style: TextStyle(
                         color: Color.fromARGB(255, 1, 86, 54), fontSize: 24),
                   ),
-                  SizedBox(
-                    height: 50,
-                    width: 260,
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Color.fromARGB(255, 0, 111, 70))),
-                        onPressed: (() async {
-                          print('I AM THE BREAK BUTTON');
-                          //Create a Map with the input data
-                          Map<String, dynamic> dataToUpdate = {
-                            'break': timeIsNow,
-                          };
-                          //update the break TimeStamp in the database with now
-                          realRef.update(dataToUpdate);
-                          //on push bring us to the timer screen
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Timer()));
-                        }),
-                        child: Text(
-                          'Go On Break',
-                          style: TextStyle(fontSize: 23),
-                        )),
-                  ),
+
+                  //This is the Go on Break Button
+                  StreamBuilder(
+                      stream: _streamBreak,
+                      builder:
+                          (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.active) {
+                          ////////////////////////////
+                          DocumentSnapshot? test = snapshot.data;
+                          ///////////////////////////////////////
+                          ///
+                          return SizedBox(
+                            height: 50,
+                            width: 260,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Color.fromARGB(255, 0, 111, 70))),
+                                onPressed: (() async {
+                                  print(
+                                      // ignore: prefer_interpolation_to_compose_strings
+                                      'this is the break activity from stream builder: ' +
+                                          test!['isBreakActive'].toString());
+                                  verify = test!['isBreakActive'];
+                                  if (verify == true) {
+                                    //since the break availabity
+                                    Map<String, dynamic> breakToUpdate = {
+                                      'isBreakActive': false,
+                                    };
+                                    breakRef.update(breakToUpdate);
+                                    print('I AM THE BREAK BUTTON');
+                                    //Create a Map with the input data
+                                    Map<String, dynamic> dataToUpdate = {
+                                      'break': timeIsNow,
+                                    };
+                                    //update the break TimeStamp in the database with now
+                                    realRef.update(dataToUpdate);
+                                    //on push bring us to the timer screen
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Timer()));
+                                  } else {
+                                    showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: null,
+                                            content: Text(
+                                                'Sorry, someone else is taking a break right now :('),
+                                          );
+                                        });
+                                  }
+                                }),
+                                child: Text(
+                                  'Go On Break',
+                                  style: TextStyle(fontSize: 23),
+                                )),
+                          );
+                        } else {
+                          return SizedBox(
+                            height: 50,
+                            width: 260,
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Color.fromARGB(255, 0, 111, 70))),
+                                onPressed: (() async {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text('no connection'),
+                                          content: Text(
+                                              'there seems to be no connection, please verify your connection and try again later'),
+                                        );
+                                      });
+                                }),
+                                child: Text(
+                                  'Go On Break',
+                                  style: TextStyle(fontSize: 23),
+                                )),
+                          );
+                        }
+                      }),
+                  // SizedBox(
+                  //   height: 50,
+                  //   width: 260,
+                  //   child: ElevatedButton(
+                  //       style: ButtonStyle(
+                  //           backgroundColor: MaterialStateProperty.all(
+                  //               Color.fromARGB(255, 0, 111, 70))),
+                  //       onPressed: (() async {
+                  //         if (canIBreak!) {
+                  //           //since the break availabity
+                  //           Map<String, dynamic> breakToUpdate = {
+                  //             'isBreakActive': false,
+                  //           };
+                  //           breakRef.update(breakToUpdate);
+                  //           print('I AM THE BREAK BUTTON');
+                  //           //Create a Map with the input data
+                  //           Map<String, dynamic> dataToUpdate = {
+                  //             'break': timeIsNow,
+                  //           };
+                  //           //update the break TimeStamp in the database with now
+                  //           realRef.update(dataToUpdate);
+                  //           //on push bring us to the timer screen
+                  //           Navigator.push(
+                  //               context,
+                  //               MaterialPageRoute(
+                  //                   builder: (context) => Timer()));
+                  //         } else {
+                  //           showDialog(
+                  //               context: context,
+                  //               builder: (BuildContext context) {
+                  //                 return AlertDialog(
+                  //                   title: null,
+                  //                   content: Text(
+                  //                       'Sorry, someone else is taking a break right now :('),
+                  //                 );
+                  //               });
+                  //         }
+                  //       }),
+                  //       child: Text(
+                  //         'Go On Break',
+                  //         style: TextStyle(fontSize: 23),
+                  //       )),
+                  // ),
                 ],
               ),
             ),
+
+            //this is the toggleable breakActivity button
+            Container(
+              alignment: Alignment(.92, .988),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 0, 111, 70))),
+                  onPressed: () async {
+                    Map<String, dynamic> breakToUpdate = {
+                      'isBreakActive': false,
+                    };
+                    breakRef.update(breakToUpdate);
+
+                    if (canIBreak!) {
+                      print('the break is true!!!!');
+                    } else {
+                      print('the break is false!!!!');
+                    }
+                  },
+                  child: Text('Toggle breakActivity')),
+            ),
+
+            //this is the Log out Button
             Container(
               alignment: Alignment(-.92, .988),
               child: ElevatedButton(
@@ -182,6 +351,18 @@ class Home extends StatelessWidget {
       });
     }
   }
+
+//function to check if someone is on break or not
+  isBreakActive() async {
+    // ignore: await_only_futures
+    await FirebaseFirestore.instance
+        .collection('Shoppers')
+        .doc('breakActivity')
+        .get()
+        .then((ds) {
+      canIBreak = ds.data()!['isBreakActive'];
+    });
+  }
 }
 
 // Text(
@@ -194,4 +375,12 @@ class Home extends StatelessWidget {
  User? user = FirebaseAuth.instance.currentUser;
     final realRef =
         FirebaseFirestore.instance.collection("Shoppers").doc(user!.uid).get();
+*/
+
+/* Future builder idea
+FutureBuilder(
+                              future: isBreakActive(),
+                              builder: (context, snapshot) {
+                                
+                              });
 */
--- lib/pages/timer.dart
+++ lib/pages/timer.dart
@@ -1,8 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:amazonbreak/pages/home.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
 
 class Timer extends StatefulWidget {
   const Timer({super.key});
@@ -13,8 +18,26 @@ class Timer extends StatefulWidget {
 
 class _TimerState extends State<Timer> {
   DateTime shift = DateTime.now();
+
+//
   var userRef = FirebaseFirestore.instance.collection('Shopper');
+
+//
   User? aUser = FirebaseAuth.instance.currentUser;
+
+//
+  var isBreakOverFireStore =
+      FirebaseFirestore.instance.collection('Shopper').doc('breakActivity');
+
+//
+  bool isBreakOver = false;
+
+//
+  bool? canIBreak;
+
+//
+  DocumentReference breakRef =
+      FirebaseFirestore.instance.collection("Shoppers").doc("breakActivity");
 
   void initState() {
     //_fetch2();
@@ -39,24 +62,72 @@ class _TimerState extends State<Timer> {
                                 ConnectionState.done) {
                               return Text('Loading....');
                             }
-                            return Text('shift time: $shift');
+                            return Text(
+                                'is breakover firestore: $isBreakOverFireStore');
                           })
                     ])),
+            Container(
+              alignment: Alignment.center,
+              //this will be the actual timer widget
+              child: TimerCountdown(
+                format: CountDownTimerFormat.minutesSeconds,
+                endTime: DateTime.now().add(Duration(minutes: 0, seconds: 30)),
+                onEnd: () {
+                  createAlertDialog(context);
+                  Map<String, dynamic> breakToUpdate = {
+                    'isBreakActive': true,
+                  };
+                  breakRef.update(breakToUpdate);
+                },
+              ),
+            ),
             Container(
               alignment: Alignment(.92, .988),
               child: ElevatedButton(
                   style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all(
-                          Color.fromARGB(255, 6, 87, 8))),
+                          Color.fromARGB(255, 0, 111, 70))),
                   onPressed: () async {},
                   child: Text('Set Break Time')),
             )
           ],
         ),
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 6, 87, 8),
-          title: Text('Timer'),
+          backgroundColor: Color.fromARGB(255, 0, 111, 70),
+          title: Text('Break Time'),
         ));
+  }
+
+  createAlertDialog(BuildContext context) {
+    return showDialog(
+        // show dialog is what helps make this work, thank god ;-;
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: null,
+            content: Text('get back to work :)'),
+            actions: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Home()));
+                  },
+                  child: Text('ok :('))
+            ],
+          );
+        });
+  }
+
+  isBreakActive() async {
+    // ignore: await_only_futures
+    await FirebaseFirestore.instance
+        .collection('Shoppers')
+        .doc('breakActivity')
+        .get()
+        .then((ds) {
+      canIBreak = ds.data()!['isBreakActive'];
+    });
   }
 
   _fetch2() async {
@@ -74,3 +145,8 @@ class _TimerState extends State<Timer> {
     }
   }
 }
+
+/*
+
+
+*/
